5bc307791f949e1ca2681312b4dbfc4af2c50473
dalaixlmao <anubhav_a@me.iitr.ac.in>
2024-07-16 21:57:05 +0530

    added next auth and signin page

---
+++ apps/user-app/lib/auth.ts
@@ -0,0 +1,54 @@
+import CredentialsProvider  from "next-auth/providers/credentials";
+import zod from 'zod';
+import { PrismaClient } from "@repo/db";
+const prisma = new PrismaClient();
+
+
+
+export const authOptions = {
+    providers: [
+      CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          email: {
+            label: "Email",
+            type: "text",
+            placeholder: "jsmit@next.com",
+          },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize(credentials: any) {
+          try {
+            const email = credentials.email;
+            const password = credentials.password;
+            const bodySchema = zod.object({
+              email: zod.string().email(),
+              password: zod.string().min(8),
+            });
+            const res = bodySchema.safeParse({
+              email: email,
+              password: password,
+            });
+            if (!res.success) return null;
+            const user = await prisma.user.findUnique({
+              where: { email: email },
+            });
+            if (!user) return null;
+            console.log(user);
+            return { id: user.id.toString() };
+          } catch (e) {
+            return null;
+          }
+  
+          return null;
+        },
+      }),
+    ],
+    secret: process.env.JWT_SECRET || "",
+    callbacks: {
+        async session({token, session}:any){
+            session.user.id = token.sub;
+            return session;
+        }
+    }
+  };
---
+++ packages/ui/src/Appbar.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+export default function Appbar(): JSX.Element {
+    return (
+      <div>Appbar</div>
+    );
+  }
+  
