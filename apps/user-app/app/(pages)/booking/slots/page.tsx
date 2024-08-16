0011f17638b9a7ad98e7e2816070a275f95dc041
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2024-08-16 17:10:08 +0530

    Update page.tsx

--- apps/user-app/app/(pages)/booking/slots/page.tsx
+++ apps/user-app/app/(pages)/booking/slots/page.tsx
@@ -1,7 +1,7 @@
 import AuditoriumStructure from "@/components/AuditoriumStructure"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
-export default async function(){
+export default async function Slots(){
     const session = await getServerSession(authOptions);
     const userId = Number(session.user.id);
 
