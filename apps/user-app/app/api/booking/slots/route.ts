c5743222249822241053399773d6f42d449db681
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2024-08-16 17:04:38 +0530

    Update route.ts

--- apps/user-app/app/api/booking/slots/route.ts
+++ apps/user-app/app/api/booking/slots/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@repo/db";
+import { PrismaClient } from "@repo/db/client";
 const prisma = new PrismaClient();
 export async function POST(req: Request) {
   const body = await req.json();
