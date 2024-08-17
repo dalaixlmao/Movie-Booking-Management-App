0ba6b7338a664e3d93f4a023a0a9c244180c085c
Anubhav <anubhav_a@me.iitr.ac.in>
2024-08-18 01:42:11 +0530

    updated slots/page.tsx

--- apps/user-app/app/(pages)/booking/slots/page.tsx
+++ apps/user-app/app/(pages)/booking/slots/page.tsx
@@ -1,11 +1,24 @@
-import AuditoriumStructure from "@/components/AuditoriumStructure"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/lib/auth"
-export default async function Slots(){
-    const session = await getServerSession(authOptions);
-    const userId = Number(session.user.id);
+import AuditoriumStructure from "@/components/AuditoriumStructure";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { Session } from "next-auth";
 
-    return <div className="mt-12 text-white">
-        <AuditoriumStructure userId = {userId} />
+interface newSession extends Session {
+  user: {
+    id: number;
+    name?: string | null | undefined;
+    email?: string | null | undefined;
+    image?: string | null | undefined;
+  };
+}
+
+export default async function Slots() {
+  const session: newSession | null = await getServerSession(authOptions);
+  const userId = Number(session?.user?.id);
+
+  return (
+    <div className="mt-12 text-white">
+      <AuditoriumStructure userId={userId} />
     </div>
+  );
 }
